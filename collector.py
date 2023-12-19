d9c4e560ee7e7329a0b4eb6c85f54a6a5e53a4c6
jez <jez.wain@atos.net>
2023-12-19 08:48:03 +0000

    rename axis = columns

--- collector.py
+++ collector.py
@@ -217,7 +217,7 @@ class Collector:
         # Use the last element of path as column/sensor name
         rename_dict = {col: col.split("/")[-1] for col in df.columns}
         print(f"{rename_dict=}")
-        df.rename(rename_dict, inplace=True)
+        df.rename(rename_dict, axis="columns", inplace=True)
         print(f"{df.columns=}")
         df.index.name = "Timestamp"
         return df

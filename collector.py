ed94ef17d8b27d6d2ffe4a64bfb0cf35c24e1401
jez <jez.wain@atos.net>
2023-12-19 08:37:58 +0000

    trace rename dict

--- collector.py
+++ collector.py
@@ -216,6 +216,7 @@ class Collector:
         df = pd.DataFrame(readings)
         # Use the last element of path as column/sensor name
         rename_dict = {col: col.split("/")[-1] for col in df.columns}
+        print(f"{rename_dict=}")
         df = df.rename(rename_dict)
         df.index.name = "Timestamp"
         return df

3cc78bad2a5b9d58743bfaba680963c4789dfae1
jez <jez.wain@atos.net>
2023-12-19 20:51:34 +0000

    Add filter method to limit # plotted lines

--- plotter.py
+++ plotter.py
@@ -25,13 +25,17 @@ class RedfishPlotter:
         )
         plt.savefig(savefilename, dpi=144)
 
-    def plot_power_sensors(self, savefilename=""):
+    def plot_power_sensors(self, savefilename="", filter=True):
         """Plot the power sensor graphs"""
 
         if savefilename == "":
             savefilename = f"{self._hostname}_power_sensors.png"
 
         df = self._dataframes["Sensors"]
+
+        if filter:
+            df = self.filter(df)
+
         power_columns = [
             col for col in df.columns if "pwr" in col.lower() or "power" in col.lower()
         ]
@@ -45,7 +49,6 @@ class RedfishPlotter:
         )
         plt.savefig(savefilename, dpi=144)
 
-
     def plot_temperature_sensors(self, savefilename=""):
         """Plot the power graphs"""
 
@@ -54,9 +57,7 @@ class RedfishPlotter:
             savefilename = f"{self._hostname}_temperature_sensors.png"
 
         df = self._dataframes["Sensors"]
-        temperature_columns = [
-            col for col in df.columns if "temp" in col.lower()
-        ]
+        temperature_columns = [col for col in df.columns if "temp" in col.lower()]
         df = df[temperature_columns]
 
         df.plot(
@@ -73,3 +74,32 @@ class RedfishPlotter:
         self._dataframes["Temperatures"].plot(
             title=f"Temperatures {self._hostname}", ylabel="Temp (ºC)", fontsize=8
         )
+        plt.savefig(savefilename, dpi=144)
+
+    def filter(self, df):
+        """
+        If # columns <= 4 return df as is, otherwise
+        retain only the columns that have values that reach at least 60% of
+        the column with the second highest max value
+        """
+
+        if len(df.columns) <= 4:
+            return df
+
+        max_values = df.max()
+        penultimate_max = sorted(max_values)[-2]
+        threshold = 0.6 * penultimate_max
+
+        # I suspect that there's a more elegant approach to this, but I can't find it...
+
+        # Get a series of bools where the index is the column name
+        pass_max = max_values > threshold
+
+        # Get a list of the column names
+        idx = pass_max.index
+
+        # Filter the list of column names if pass_max is true for column
+        pass_cols = [idx[i] for i, v in enumerate(pass_max)]
+
+        # return dataframe with the passing columns
+        return df[pass_cols]

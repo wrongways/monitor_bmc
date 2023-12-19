787771ac42dbfcbedf3f818e03608d55bf858f72
jez <jez.wain@atos.net>
2023-12-19 10:30:11 +0000

    Can’t use self as a default param in mehtod sig.

--- plotter.py
+++ plotter.py
@@ -6,7 +6,17 @@ class RedfishPlotter:
         self._dataframes = dataframes
         self._hostname = hostname
 
-    def plot_power(self, savefilename=f"{self._hostname}_power.png"):
+    def plot_power(self, savefilename=""):
+        """Plot the power graphs
+
+        If the savefilename is empty, use hostname_power.png - can't use
+        "self" here as a default value
+        """
+
+        # Setup default savefilename - can't use self in the default
+        if savefilename == "":
+            savefilename = f"{self._hostname}_power.png"
+
         df = self._dataframes["Power"]
         df.plot(
             title=f"Power Draws {self._hostname}",
